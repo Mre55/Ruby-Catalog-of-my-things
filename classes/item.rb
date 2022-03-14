f2f38ef5124af64bf8a72e658d2f11d5561c110c
kerolous samy <keroloussamy98@gmail.com>
2022-03-14 22:24:40 +0200

    Add item class

---
+++ classes/item.rb
@@ -0,0 +1,37 @@
+class Item
+  
+  def initialize(publish_date, archived)
+    @id = Random.rand(1..1000)
+    @genre
+    @author
+    @source
+    @label
+    @publish_date
+    @archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
+  def can_be_archived?
+    return true if @publish_date < 2012
+    return false if @publish_date > 2012
+  end
+
+  def move_to_archive()
+    if can_be_archived? @archived = true
+  end
+end
