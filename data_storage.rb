423e6419c2f6d68484f4239de729c4399042e12e
kerolous samy <keroloussamy98@gmail.com>
2022-03-15 19:48:05 +0200

    Add DataStorage module

---
+++ data_storage.rb
@@ -0,0 +1,24 @@
+require './classes/game'
+require 'json'
+
+module DataStorage
+  def read_games
+    file = './json_data/games-data.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(Game.new(element['publish_date'], element['archived'], element['multiplayer'], element['last_played_at'], id: element['id'].to_i))
+      end
+    end
+    data
+  end
+
+  def save_games(games)
+    data = []
+    games.each do |game|
+      data.push({ id: game.id, publish_date: game.publish_date, archived: game.archived, multiplayer: game.multiplayer, last_played_at: game.last_played_at })
+    end
+    File.write('./json_data/games-data.json', JSON.generate(data))
+  end
+
+end
