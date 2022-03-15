66a841e6b52c00f12b3bccc1cafce74d29012877
mre55 <misganawmihreteab>
2022-03-16 02:29:28 +0300

    Add book data method to preserved by saving collections in of books in book-data.json file

--- data_storage.rb
+++ data_storage.rb
@@ -1,7 +1,28 @@
 require './classes/game'
+require './classes/book'
 require 'json'
 
 module DataStorage
+  def read_books
+    file = './json_data/book-data.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(Book.new(element['publish_date'], element['archived'], element['publisher'], element['cover_state'],
+                           id: element['id'].to_i))
+      end
+    end
+    data
+  end
+
+  def save_books(books)
+    data = []
+    books.each do |book|
+      data.push({ id: book.id, publisher: book.publisher, cover_state: book.cover_state })
+    end
+    File.write('./json_data/book-data.json', JSON.generate(data))
+  end
+
   def read_games
     file = './json_data/games-data.json'
     data = []
