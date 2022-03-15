b833023360f35e2ec80abeec690bfc36b725d144
kerolous samy <keroloussamy98@gmail.com>
2022-03-15 16:57:41 +0200

    use date.parse

--- classes/item.rb
+++ classes/item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
   attr_accessor :genre, :author, :source, :label
 
@@ -24,8 +26,11 @@ class Item
   end
 
   def can_be_archived?
-    return true if @publish_date < 2012
-    return false if @publish_date > 2012
+    if Date.parse(@publish_date) < Date.parse("2012-01-1")
+      return true;
+    else
+      return false
+    end
   end
 
   def move_to_archive()
