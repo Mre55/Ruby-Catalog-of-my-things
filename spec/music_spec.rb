1dfdc2a159a8193234ed59ab1957265ae78805a5
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-16 10:23:26 -0500

    Implement test for music_album class.

---
+++ spec/music_spec.rb
@@ -0,0 +1,21 @@
+require './classes/music_album'
+
+describe MusicAlbum do
+  before :each do
+    @musicalbum = MusicAlbum.new('2022-03-15', true, true)
+    @musicalbumtwo = MusicAlbum.new('2022-03-15', true, false)
+  end
+
+  context 'When MusicAlbum is on_spotity to true' do
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(@musicalbum).to be_an_instance_of MusicAlbum
+    end
+  end
+
+  context 'When MusicAlbum is on_spotity to false' do
+    it 'Should return an instance of the Class musicAlbum' do
+      expect(@musicalbumtwo).to be_an_instance_of MusicAlbum
+    end
+  end
+  
+end
