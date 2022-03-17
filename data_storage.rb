9f30ebe137dcf7aaedb915138fe916b80026eed5
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-17 08:01:10 -0500

    Implement display methods for MusicAlbums and Genres and for Music Albums only save method.

--- data_storage.rb
+++ data_storage.rb
@@ -2,9 +2,34 @@ require './classes/game'
 require './classes/author'
 require './classes/book'
 require './classes/label'
+require './classes/music_album'
+require './classes/genre'
 require 'json'
 
 module DataStorage
+  def read_music_albums
+    file = './json_data/music_albums-data.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(MusicAlbum.new(element['publish_date'], element['archived'], element['name'], element['on_spotify'],
+                           id: element['id'].to_i))
+      end
+    end
+    data
+  end
+
+  def read_genres
+    file = './json_data/genres-data.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(Genre.new(element['name']))
+      end
+    end
+    data
+  end
+
   def read_books
     file = './json_data/book-data.json'
     data = []
@@ -12,6 +37,17 @@ module DataStorage
       JSON.parse(File.read(file)).each do |element|
         data.push(Book.new(element['publish_date'], element['archived'], element['publisher'], element['cover_state'],
                            id: element['id'].to_i))
+      end
+    end
+    data
+  end
+
+  def read_labels
+    file = './json_data/labels-data.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |element|
+        data.push(Label.new(element['title'], element['color']))
       end
     end
     data
@@ -46,6 +82,15 @@ module DataStorage
       end
     end
     data
+  end
+
+  def save_music_albums(music_albums)
+    data = []
+    music_albums.each do |music_album|
+      data.push({ id: music_album.id, publish_date: music_album.publish_date, archived: music_album.archived, name: music_album.name,
+                  on_spotify: music_album.on_spotify })
+    end
+    File.write('./json_data/music_albums-data.json', JSON.generate(data))
   end
 
   def save_games(games)
