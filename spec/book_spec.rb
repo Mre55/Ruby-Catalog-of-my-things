3354b83c4d100665a75cbe2c859add855e3346d5
kerolous samy <keroloussamy98@gmail.com>
2022-03-17 18:06:27 +0200

    Update test can_be_archived method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,17 +11,8 @@ describe Book do
   it 'creates an instance of an Item' do
     expect(@book).to be_a Item
   end
-  describe '#can_be_archived?' do
-    it 'should return true if cover_state equals to "bad"' do
-      bad_cover_book = Book.new('2020-10-25', true, 'Addis Ababa Press', 'bad')
-      expect(bad_cover_book.can_be_archived?).to be true
-    end
-    it 'should return true if parents method returns true' do
-      old_book = Book.new('1985-10-09', true, 'Addis Ababa University Press', 'good')
-      expect(old_book.can_be_archived?).to be true
-    end
-    it 'otherwise should return false' do
-      expect(@book.can_be_archived?).to be false
-    end
+  it 'The move_to_archive? method should return true' do
+    game = Book.new('2009-01-1', true, 'Addis Ababa University Press', 'good')
+    expect(game.move_to_archive).to be true
   end
 end
